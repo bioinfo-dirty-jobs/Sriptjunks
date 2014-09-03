c1d268488cb29e236d71b2a742e312dcd9d5c848
bioinfo-dirty-jobs <mauriziopolano@blu.it>
2014-09-03 15:49:44 +0200

    3settembre2014

--- junkls/Sam.Prep_sort.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-input="accepted_hits.bam"
-lista_nomi=(`ls -d */`)
-tm=$(date);
-echo "# $tm";
-for i in "${lista_nomi[@]}";do Nam=$(echo $i|sed "s/_ens\///g");
-echo samtools sort  $i$input   $i$Nam"_sort";done
-
-
